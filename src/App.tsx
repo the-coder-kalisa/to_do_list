19d1c535fc62ad8f6127100c1b3c1a54ec110408
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-11-19 21:15:38 +0200

    finished table

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,43 @@
 import { FC } from "react";
 import AddTask from "./components/AddTask";
+import { demoTasks } from "./data/demo_tasks";
 import Logo from "./subcomponents/Logo";
 
 const App: FC = () => {
   return (
     <div className="container">
       <Logo />
-      <AddTask />
-      <div className="tasks_operations"></div>
+      <div className="tasks_operations">
+        <AddTask />
+        <table>
+          <tr className="header">
+            <th>
+              <input type="checkbox" />
+            </th>
+            <th>Task</th>
+            <th>Status</th>
+            <th>Operations</th>
+          </tr>
+          {demoTasks.map(({ status, task }, index) => (
+            <tr key={index}>
+              <td>
+                <input type="checkbox" />
+              </td>
+              <td>{task}</td>
+              <td>
+                <div className={`${status} status`}>
+                  <div className="ellipsis"></div>
+                  <div>{status.toUpperCase()}</div>
+                </div>
+              </td>
+              <td>
+                <button>Edit</button>
+                <button>Delete</button>
+              </td>
+            </tr>
+          ))}
+        </table>
+      </div>
     </div>
   );
 };
---
+++ src/data/demo_tasks.ts
@@ -0,0 +1,12 @@
+import { Task } from "./types";
+
+export const demoTasks : Task[] = [
+  {
+    task: "Eating",
+    status: "pending",
+  },
+  {
+    task: "Sleeping",
+    status: "completed",
+  },
+];
